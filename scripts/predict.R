5c500a10bc7cffb0c357ba65e559108fa19d03ae
Doug Callaway <dcal@protonmail.com>
2016-09-07 14:16:07 +1200

    improved knn algorithm

--- scripts/predict.R
+++ scripts/predict.R
@@ -1,3 +1,17 @@
-nearestNeighbor = function(similarities, outcomes) {
-  outcomes[which.max(similarities)]  
+knn.similarityWeighted = function(similarities, outcomes, k = 1) {
+  ## K-Nearest Neighbor
+  ## if k > 1, returns proximity-weighted average of k-nearest neighbors
+  
+  kNearest = head(order(-similarities), k) # closest to furthest
+  weights = similarities[kNearest]
+  
+  if(k == 1) return(outcomes[kNearest]) # nearest-neighbor
+  
+  if(is.factor(outcomes)) {
+    # similarity-weighted voting scheme
+    vote = weighted.mean(as.integer(outcomes[kNearest]), weights)
+    levels(outcomes)[round(vote)]
+  }
+  
+  else round(weighted.mean(outcomes[kNearest], weights), 2)
 }
